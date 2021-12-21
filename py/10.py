61836ed65b7762dc6ba044490c246658d01c4113
Gleb Buzin <qufiwefefwoyn@gmail.com>
2021-12-22 02:50:25 +0300

    wip

--- py/10.py
+++
@@ -1,8 +0,0 @@
-x = Symbol('x')
-n = Dummy('n', real = True)
-solveset(3 - Mod(x, 5), x)
-# ImageSet(Lambda(n, 5*n + 3), S.Integers)
-solveset(3 - Mod(x + 2, 5), x)
-# ImageSet(Lambda(n, 5*n + 1), S.Integers)
-solveset(3 - Mod(5*x, 7), x)
-# ImageSet(Lambda(n, 7*n + 2), S.Integers)
--- py/8.py
+++
@@ -1,5 +0,0 @@
-if mainpow and symbol in mainpow.exp.free_symbols:
-    lhs = collect(lhs, mainpow)
-    if lhs.is_Mul and rhs != 0:
-        soln = _lambert(expand_log(log(abs(lhs)) - log(abs(rhs))), symbol)
-# see the abs used.
